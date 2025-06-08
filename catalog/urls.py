4047c84e7d83657d4525c62a9248d1fd3c5f0140
Marina <lav_mari@mail.ru>
2025-06-08 06:03:07 +0300

    4/2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home, product_details, product_list
+from catalog.views import HomeTemplateView, ProductDetailView, ProductListView, contacts
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', home, name='home'),
+    path('home/', HomeTemplateView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('', product_list, name='product_list'),
-    path('product/<int:pk>/', product_details, name='product'),
+    path('', ProductListView.as_view(), name='product_list'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView, TemplateView
 from .models import Product
 
 
-def home(request):
-    return render(request,'home.html')
+class HomeTemplateView(TemplateView):
+    template_name = 'catalog/home.html'
 
 
 def contacts(request):
@@ -18,13 +19,20 @@ def contacts(request):
     return render(request,'contacts.html')
 
 
-def product_list(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, template_name='product_list.html', context=context)
+#def product_list(request):
+    #products = Product.objects.all()
+    #context = {'products': products}
+    #return render(request, template_name='product_list.html', context=context)
 
 
-def product_details(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, template_name='product_details.html', context=context)
+#def product_details(request, pk):
+    #product = get_object_or_404(Product, pk=pk)
+    #context = {'product': product}
+    #return render(request, template_name='product_detail.html', context=context)
+
+class ProductListView(ListView):
+    model = Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
